8187bc1a3829d9d4a987f04504fbde47ec662316
Lasse Lund-Egmose <llund-egmose@hotmail.com>
2020-06-15 16:23:19 +0200

    Add VaultItem table

--- app/src/main/java/dk/dtu/PassVault/Business/Database/RoomDatabase.java
+++ app/src/main/java/dk/dtu/PassVault/Business/Database/RoomDatabase.java
@@ -3,10 +3,13 @@ package dk.dtu.PassVault.Business.Database;
 import androidx.room.Database;
 
 import dk.dtu.PassVault.Business.Database.DAO.CredentialDao;
+import dk.dtu.PassVault.Business.Database.DAO.VaultItemDao;
 import dk.dtu.PassVault.Business.Database.Entities.Credential;
+import dk.dtu.PassVault.Business.Database.Entities.VaultItem;
 
-@Database(entities = {Credential.class}, version = 1)
+@Database(entities = {Credential.class, VaultItem.class}, version = 1)
 public abstract class RoomDatabase extends androidx.room.RoomDatabase {
 
     public abstract CredentialDao credentialDao();
+    public abstract VaultItemDao vaultItemDao();
 }
