2f9349a70bf381e913b5775bdbf25849581d9e7f
NKKH <47367750+NKKH@users.noreply.github.com>
2020-06-15 11:45:18 +0200

    Update MainActivity.java

--- app/src/main/java/dk/dtu/PassVault/MainActivity.java
+++ app/src/main/java/dk/dtu/PassVault/MainActivity.java
@@ -1,14 +1,28 @@
 package dk.dtu.PassVault;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generatepass);
+        setContentView(R.layout.activity_main);
+
+        Button signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                
+            }
+        });
     }
 }
