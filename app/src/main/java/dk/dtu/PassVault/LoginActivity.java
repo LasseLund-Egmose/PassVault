5213c777effb14226a4611c7207d504e64edc5e8
Lasse Lund-Egmose <llund-egmose@hotmail.com>
2020-06-16 20:18:52 +0200

    Minimalistic login screen concept + svg logo

--- app/src/main/java/dk/dtu/PassVault/LoginActivity.java
+++ app/src/main/java/dk/dtu/PassVault/LoginActivity.java
@@ -53,7 +53,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.clean_login);
         getSupportActionBar().hide();
 
         Button signInButton = findViewById(R.id.signInButton);
