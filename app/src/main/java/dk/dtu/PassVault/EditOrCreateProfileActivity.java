44fabf6c2651cacfee7107cc070e35a270e1bc14
Lasse Lund-Egmose <llund-egmose@hotmail.com>
2020-06-16 09:51:07 +0200

    Make "Generate" btn open PasswordGeneratorActivity

--- app/src/main/java/dk/dtu/PassVault/EditOrCreateProfileActivity.java
+++ app/src/main/java/dk/dtu/PassVault/EditOrCreateProfileActivity.java
@@ -3,8 +3,10 @@ package dk.dtu.PassVault;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 
 public class EditOrCreateProfileActivity extends AppCompatActivity {
@@ -15,5 +17,11 @@ public class EditOrCreateProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_add);
+
+        Button genBtn = (Button) findViewById(R.id.generate_password);
+        genBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(),PasswordGeneratorActivity.class);
+            startActivity(intent);
+        });
     }
 }
