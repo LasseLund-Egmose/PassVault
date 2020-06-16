4881dac83e9263e4dd2ebe0643f18e6803eb79df
NKKH <47367750+NKKH@users.noreply.github.com>
2020-06-16 11:14:40 +0200

    Landscape XML done

--- app/src/main/java/dk/dtu/PassVault/LoginActivity.java
+++ app/src/main/java/dk/dtu/PassVault/LoginActivity.java
@@ -17,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
 
         Button signInButton = findViewById(R.id.signInButton);
         signInButton.setOnClickListener(new OnClickListener(){
