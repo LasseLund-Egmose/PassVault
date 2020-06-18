da5b92505b054766294c881de3818376ff592b14
Lasse Lund-Egmose <llund-egmose@hotmail.com>
2020-06-18 12:02:31 +0200

    Fix Room schema error

--- app/src/main/java/dk/dtu/PassVault/Business/Database/RoomDatabase.java
+++ app/src/main/java/dk/dtu/PassVault/Business/Database/RoomDatabase.java
@@ -7,7 +7,7 @@ import dk.dtu.PassVault.Business.Database.DAO.VaultItemDao;
 import dk.dtu.PassVault.Business.Database.Entities.Credential;
 import dk.dtu.PassVault.Business.Database.Entities.VaultItem;
 
-@Database(entities = {Credential.class, VaultItem.class}, version = 1)
+@Database(entities = {Credential.class, VaultItem.class}, version = 1, exportSchema = false)
 public abstract class RoomDatabase extends androidx.room.RoomDatabase {
 
     public abstract CredentialDao credentialDao();
